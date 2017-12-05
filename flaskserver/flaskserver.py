78cbe50ddc6a24357c4dc6858b0d08e934dc8036
sodium49 <nooryyaa2@naver.com>
2017-12-05 10:39:38 -0800

    flask server updated

--- flaskserver/flaskserver.py
+++ flaskserver/flaskserver.py
@@ -1,26 +1,54 @@
+# -- coding: utf-8 --
 #!flask/bin/python
 import sys
+import os
+from flask import Flask, send_from_directory, render_template, request, redirect, Response, url_for
+from werkzeug import secure_filename, SharedDataMiddleware
 
-from flask import Flask, render_template, request, redirect, Response
-import random, json
+UPLOAD_FOLDER='./analyzed/pcap'
+ALLOWED_EXTENSIONS = set(['pcap', 'pcapng'])
 
 app = Flask(__name__)
-
-@app.route('/')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+"""
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
+"""
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
 	# serve index template
-	return render_template('test.html', name='Joe')
 
-@app.route('/hello')
-def hellopy():
+	return render_template('index.html', name='Joe')
 
-	return "hello"
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		if('file' not in request.files or 
+			request.form['casename'] == ''):
+			return redirect(url_for('index'))
+
+		print request.form
+		pcapfile = request.files['file']
+		reqCasename = request.form['casename']
+
+		if('do-isp' in request.form):
+			doIsp = request.form['do-isp']
+		#print doIsp
+		filename = secure_filename(pcapfile.filename)
+		pcapfile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+		return redirect(url_for('show_result_page', casename=reqCasename))
 
 @app.route('/result_page')
-@app.route('/<testcase>')
-def show_result_page(testcase=None):
+@app.route('/result_page/<casename>')
+def show_result_page(casename=None):
+	print casename
+	return render_template('result.html', casename=casename)
 
-	return resnder_template('result.html', testcase=testcase)
 
 @app.route('/receiver', methods = ['POST'])
 def worker():
@@ -33,6 +61,17 @@ def worker():
 		result += str(item['make']) + '\n'
 
 	return result
+
+
+
+
+
+@app.route('/hello')
+def hellopy():
+
+	return "hello"
+
+
 
 if __name__ == '__main__':
 	# run!
---
+++ flaskserver/static/filter-bar.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("filter-bar").style.right = "0px";
+}
+
+function closeNav() {
+    document.getElementById("filter-bar").style.right = "-250px";
+}
