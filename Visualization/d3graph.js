2daffef2f8568c0af7c0898a2688cecb9c34dc08
sangkimbin <tkdqls18@gmail.com>
2017-12-06 22:21:48 +0900

    visualization updated

--- Visualization/d3graph.js
+++ Visualization/d3graph.js
@@ -1,6 +1,5 @@
-var svgWidth = 800;	// SVG 요소의 넓이
-var svgHeight = 400;	// SVG 요소의 높이
-
+var svgWidth = 800;
+var svgHeight = 400;
 
 var margin = {top: 40, right: 40, bottom: 60, left: 60},
 		width = svgWidth - margin.left - margin.right,
@@ -8,9 +7,8 @@ var margin = {top: 40, right: 40, bottom: 60, left: 60},
 
 var scale = height/100;
 
-//일단 아무값이나
 var average_tick=10;
-
+// something data
 var dataSet = [
 	{"time":'10:00',"value":0},
 	{"time":'10:01',"value":20},
@@ -21,8 +19,9 @@ var dataSet = [
 	{"time":'10:06',"value":40},
 	{"time":'10:10',"value":20},
 	{"time":'10:11',"value":40},
-	{"time":'10:12',"value":90}
-
+	{"time":'11:12',"value":90},
+	{"time":'12:13',"value":30},
+	{"time":'13:14',"value":60}
 ];
 
 var parseDate = d3.time.format('%H:%M').parse;
@@ -34,59 +33,62 @@ for(var i=0;i<dataSet.length;i++){
 }
 
 drawScale();
-drawGraph(dataSet, "graphline", "cardinal");	// 곡선으로 표시
+drawGraph(dataSet, "graphline", "cardinal");	// as curve line
 
-// 그래프를 표시하는 함수
 function drawGraph(dataSet, cssClassName, type){
-	// 색칠 공부
-	var area = d3.svg.area()	// svg 영역
-	  .x(function(d, i){
-			return margin.left + i * ( width / (dataSet.length-1) );	// X 좌표는 표시 순서×간격
+	// fill color
+	var area = d3.svg.area()	// svg area
+		.x(function(d, i){
+			return margin.left + i * ( width / (dataSet.length-1) );
 		})
 	  .y0(function(d, i){
-			return svgHeight - margin.bottom;	// 데이터로부터 Y 좌표 빼기
+			return svgHeight - margin.bottom;
 		})
 	  .y1(function(d, i){
-			return svgHeight - d["value"] * scale  - margin.bottom;	// 데이터로부터 Y 좌표 빼기
+			return svgHeight - d["value"] * scale  - margin.bottom;	// data - y axis
 		})
-	  .interpolate(type)	//  그래프의 모양 설정
+	  .interpolate(type)	//  type of shape graph line
 
-	// 그래프 그리기
+	// draw graph
 	var lineElements = d3.select("#myGraph2")
-	  .append("path")	// 데이터 수만큼 path 요소가 추가됨
-	  .attr("class", "line "+cssClassName)	// CSS 클래스 지정
+	  .append("path")	// add the number of data as path
+	  .attr("class", "line "+cssClassName)	// CSS class
 		.transition()
-		.duration(2000)
-	  .attr("d", area(dataSet))	//연속선 지정
+		.duration(3000)
+		 .style("fill","#154360")
+	  .attr("d", area(dataSet))
 
+		// delete data
 		lineElements
 			.remove()
 }
 
 function drawScale(){
-
-//tick 기준 정할때 사용
+	// standard tick
 	var average_tick_help=d3.extent ( ddata , function ( d ) { return d['time'] ; } );
 	average_tick = (average_tick_help[1]-average_tick_help[0]) / ( 60 * 1000 * 9);
-
 	// create an svg container
 	var vis = d3.select("#myGraph2")
 		.append("svg:svg")
 		.attr("width", svgWidth)
 		.attr("height", svgHeight);
 
+	// delete x text
+	vis.selectAll(".xaxis text")
+	.remove()
+
 	var yScale = d3.scale.linear()
 		.domain([0, 1])
 		.range([height,0]);
 
 	var xScale = d3.time.scale()
-		.domain( d3.extent ( ddata , function ( d ) { return d['time'] ; } ) )  // [최소,최대]
+		.domain( d3.extent ( ddata , function ( d ) { return d['time'] ; } ) )  // [minimum,maximum]
 		.range([0,width]);
 
 	var yAxis = d3.svg.axis()
-			.ticks(6)
-			.tickFormat(formatPercent)
-			.orient("left")
+			.ticks(6) // the number of x value
+			.tickFormat(formatPercent) // mark %
+			.orient("left") //location y axis
 			.scale(yScale);
 
 	var xAxis = d3.svg.axis()
@@ -94,20 +96,16 @@ function drawScale(){
 			.orient("bottom")
 			.scale(xScale)
 
-
 	vis.append("g")
 			.attr("transform", "translate("+margin.left+","+margin.top+")")
 			.call(yAxis);
-
 
 	vis.append("g")
 			.attr("class", "xaxis")
 			.attr("transform", "translate("+margin.left+"," + (height+margin.top)  + ")")
 			.call(xAxis);
 
-
-///x축 텍스트 적기
-
+			// x axis text
 	vis.selectAll(".xaxis text")  // select all the text elements for the xaxis
 			.transition()
 			.duration(2000)
@@ -116,23 +114,17 @@ function drawScale(){
 			.attr("dy","0.7em")
 			.style("text-anchor","start")
 
-
-
-	//그리드
-
+			// for grid
 	var grid = d3.select("#myGraph2").append("g")
-
-	var rangeX = d3.range( margin.left , width , 70 );
+	var rangeX = d3.range( margin.left , width , 70 ); // grid interval
 	var rangeY = d3.range( margin.bottom , height , 70 );
 
-
-	grid.selectAll("line.y")	// line요소의 y 클래스를 선택
-	  .data(rangeY)	// 세로 방향의 범위를 데이터셋으로 설정
+	grid.selectAll("line.y")	// y class of line
+	  .data(rangeY)	// y axis range
 	  .enter()
-	  .append("line")	// line 요소 추가
-	  .attr("class", "grid")	// CSS 클래스의 grid를 지정
-		// (x1,y1)-(x2,y2)의 좌표값을 설정
-	  .attr("x1", margin.left)              //x양 끝 틀
+	  .append("line")	// add css line class
+		.attr("class", "grid")
+	  .attr("x1", margin.left)
 	  .attr("y1", function(d, i){
 			return svgHeight - d - margin.bottom;
 		})
@@ -140,41 +132,49 @@ function drawScale(){
 	  .attr("y2", function(d, i){
 			return svgHeight - d - margin.bottom;
 		})
-	// 가로 방향의 그리드 생성
-	grid.selectAll("line.x")	// line요소의 x 클래스를 선택
-	  .data(rangeX)	// 가로 방향의 범위를 데이터셋으로 설정
+
+	grid.selectAll("line.x")
+	  .data(rangeX)
 	  .enter()
-	  .append("line")	// line 요소 추가
-	  .attr("class", "grid")	// CSS 클래스의 grid를 지정
-		// (x1,y1)-(x2,y2)의 좌표값을 설정
+	  .append("line")
+	  .attr("class", "grid")
 	  .attr("x1", function(d, i){
 			return d + margin.left;
 		})
-	  .attr("y1", svgHeight - margin.bottom)   //y 양끝
+	  .attr("y1", svgHeight - margin.bottom)
 	  .attr("x2", function(d, i){
 			return d + margin.left;
 		})
 	  .attr("y2", svgHeight -margin.bottom - height)
-
 }
-
+/*
 function updateData(data){
+	var reserve = [];
 	var result = [];
-	var last_result = [];
-	var time=0,value=0;
+	var time ;
+	var value ;
 
-	for(var i=0;i<data.length;i++){
+	for(var i=0 ; i < dataSet.length ; i++){
+		reverse=[];
 		time = Math.random() * 60;
 		value = Math.random() * 100;
-		result={"time" : "10:"+time+"" ,"value" : value};
-		last_result.push({"time" : parseDate(result["time"]) ,"value" : result["value"]});
+		reserve={"time" : "10:"+time+"" ,"value" : value};
+		result.push({"time" : parseDate(reserve["time"]) ,"value" : reserve["value"]});
 	}
-	return last_result;
+	return result
 }
-
+*/
 
 setInterval(function(){
- 	ddata = updateData(dataSet);
+/*
+	ddata = updateData(dataSet);
+*/
+	ddata = 	dataSet1 = d3.json("asdf.json", function(data){
+			var datas=[];
+			for(var i=0;i<datas.length;i++){
+				datas.push(data[i]["time"],data[i]["value"]);
+			}return datas;
+		})
 	drawScale();
 	drawGraph(ddata, "graphline", "cardinal");
-}, 2000);
+}, 3000);
--- Visualization/d3map.js
+++ Visualization/d3map.js
@@ -1,14 +1,14 @@
 var svgWidth = 800;
 var svgHeight = 400;
 
-var dataSet = [
+var dataSet1 = [
 		[1, 1], [2, 2], [3, 3], [4, 4], [1, 1],
 		[1, 1], [1, 1], [1, 1], [1, 1], [1, 1]
 	];
 // start format
 var circleElements = d3.select("#myGraph")
   .selectAll("circle")
-  .data(dataSet)
+  .data(dataSet1)
 
 circleElements
   .enter()
@@ -21,8 +21,8 @@ circleElements
 		return svgHeight-d[1];	// y axis
 	})
   .attr("r", 5)	// circle size
-
-function updateData(data){
+/*
+function updateData1(data){
 	var result = data.map(function(d, i){	// count as many as the number of data
 		var x = Math.random() * svgWidth;  // random x
 		var y = Math.random() * svgHeight;  // radom y
@@ -30,10 +30,12 @@ function updateData(data){
 	})
 	return result;
 }
+*/
+
 // renewal
-function updateGraph(){
+function updateGraph1(){
 	circleElements
-	  .data(dataSet)
+	  .data(dataSet1)
 	  .transition()	// animation
     .duration(2000)  // ani 2 second
     .attr("cx", function(d, i){
@@ -49,6 +51,15 @@ function updateGraph(){
 }
 // timer two second interval
 setInterval(function(){
-	dataSet = updateData(dataSet);	// new data
-	updateGraph();	// new graph
+/*
+	dataSet1 = updateData1(dataSet1);	// new data
+*/
+	dataSet1 = d3.json("asdf.json", function(data){
+		var datas=[];
+		for(var i=0;i<datas.length;i++){
+			datas.push(data[i][0],data[i][1]);
+		}return datas;
+	})
+
+	updateGraph1();	// new graph
 }, 2000);
